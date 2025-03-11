45dbefa7e3827ff942148a50f5d5bce22227610c
praveen m <praveengouranga@gmail.com>
2025-03-11 14:41:41 +0000

    tbl control 2tab completed

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -1,22 +1,39 @@
 sap.ui.define([
     
-    "oft/fiori/nov/controller/BaseController"
-], function(BaseController) {
+    "oft/fiori/nov/controller/BaseController",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast"
+
+], function(BaseController,MessageBox,MessageToast) {
     "use strict";
 
     return BaseController.extend("oft.fiori.nov.controller.View2", {
         onInit() {
         },
 
-
-
-
-        onBack: function(){
+  onBack: function(){
             //  get object of parent for the both view:container control
             var oApp =  this.getView().getParent();
             //  call the method to navigate to another view by passing id
             oApp.to("idView1");
+        },
+
+onApprove: function(){
+    MessageBox.confirm("did you really like this fruit",{
+         onClose: function(status){
+        if(status==="OK"){
+            MessageToast.show("GOOD")
+
+
         }
+
+
+         }
+
+    })
+}
+
+
 
 
 
